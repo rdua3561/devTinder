b97f158e50f92e93d84054c1d82157885c1b5710
rdua3561 <rdua3561@gmail.com>
2024-10-17 15:51:53 +0530

    some notes while cheating express server

--- src/app.js
+++ src/app.js
@@ -2,18 +2,20 @@ const express = require("express");
 const app= express();
 
 
-
+//Once you created the server then only you can listen to the incomming requests
 app.listen(7777, ()=>{
 
     console.log("server is successfully listening to port 7777")
 })
 
+//Request Handler for /test
 app.use("/test",(req,res)=>{
 
     res.send("hello from the server");
 
 });
 
+//Request Handler
 app.use("/",(req,res)=>{
 
     res.send("server");
