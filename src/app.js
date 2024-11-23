4cad9f2df7b0f38388f4d647692a596a28d99e7e
rdua3561 <rdua3561@gmail.com>
2024-11-23 06:40:16 +0530

    Encrypting the password  bcrypt.hash Library and stored it in DB + Made signUp and Login API's

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ const app= express();
 //const {adminAuth, userAuth} = require("./Middlewares/auth");
 const connectDB = require("./config/database");
 const User = require("./models/user");
+const {validateSignUpData} = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());// middleware that convert JSON object to Javascript Object for whole code (Globally) and then add to req
 app.post("/signup", async (req,res)=>{ //Pushing the data to the database
 
+  
     console.log(req.body);
     // const userObj={
     //     firstName: "MS",
@@ -17,19 +20,68 @@ app.post("/signup", async (req,res)=>{ //Pushing the data to the database
 
     // creating a new Instance of a User Model and passing the data(userObj) 
     // const user = new User(userObj);
-    const user = new User(req.body);// req.body will be converted to Javascript Object using middleware
+   
 
     try {
+       
+        //Validation of Data
+          validateSignUpData(req);
 
+        
+        const {firstName, lastName, emailId, password} = req.body; // req.body will be converted to Javascript Object using middleware
+
+        // Encrypt the Password
+        const passwordHash = await bcrypt.hash(password,10); // creating PasswordHash
+        console.log(passwordHash);
+
+        //creating a new instance of the user Model
+        const user = new User({
+
+        firstName,
+        lastName,
+        emailId,
+        password: passwordHash //storing  PasswordHash
+
+        });
         await user.save(); // saving to database
         res.send("User added successfully");
 
     } catch(err)
     {
-        res.status(400).send("Error Saving the user:" + err.message)
+        res.status(400).send("ERROR: " + err.message)
     }
     
 })
+
+app.post("/login", async (req, res)=>{
+    try{
+        const {emailId, password}=req.body;
+
+        const user = await User.findOne({emailId:emailId});
+
+        if(!user)
+        {
+            throw new Error("Invalid Credentials");// email not present in database
+        }
+
+        const isPasswordValid=await bcrypt.compare(password, user.password);
+
+        if(isPasswordValid)
+        {
+            res.send("User Logged In Successfully");
+        }
+        else
+        {
+            throw new Error("Invalid Credentials"); //password incorrect
+        }
+     
+      
+    }
+
+    catch(err){
+        res.status(400).send("Error LoggedIn the user:" + err.message)
+    }
+});
 
 //Get particular user by email
 app.get("/user", async (req, res)=>{
